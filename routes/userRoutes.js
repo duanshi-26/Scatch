e575550294cf034fabcaf0c25c2a670a9552df92
duanshi-26 <duanshichawla2612@gmail.com>
2025-09-01 12:56:49 +0530

    final changes done

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express();
+const {registerUser , loginUser , logoutUser} = require("../controllers/authUserController");
+
+
+router.post('/register' , registerUser)
+router.post('/login' , loginUser)
+router.get('/logout' , logoutUser)
+
+
+module.exports = router;
